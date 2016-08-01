64f6f328a0b37facb90cba3d3cccf1d7945576b7
DI-access <direction-innovation@cned.fr>
2016-08-01 17:53:08 +0200

    Update iat.js

--- iat.js
+++ iat.js
@@ -761,7 +761,7 @@ define(['pipAPI','pipScorer'], function(APIConstructor,Scorer) {
 
 				DScoreObj = scorer.computeD();
 
-				media = {css:{color:'black'},media:{html:'<div><p style="font-size:18px;color:#FFFFFF"> '+DScoreObj.FBMsg+'<br><span style="color:#56C2ED;">Votre résultat :'+DScoreObj.DScore+'</span></p></div><div><p style="font-size:28px;color:#FFFFFF">Vous avez terminé le test<br/><br/>Vous pouvez poursuivre le programme AccessiPro en cliquant sur ce lien :<br/><br/> <a href="http://bit.ly/2arXTSD" title="Retour au programme de sensibilisation au handicap AccessiPro">Retour au programme de sensibilisation au handicap AccessiPro</a></p></div>'}};
+				media = {css:{color:'black'},media:{html:'<div><p style="font-size:14px;color=#FFFFFF>'+DScoreObj.FBMsg+'<br><span style="color:#56C2ED;">Votre résultat :'+DScoreObj.DScore+'</span><br/><br/></p><p style="font-size:14px;color:#FFFFFF">Vous avez terminé le test<br/><br/>Vous pouvez poursuivre le programme AccessiPro en cliquant sur ce lien :<br/><br/> <a href="http://bit.ly/2arXTSD" title="Retour au programme de sensibilisation au handicap AccessiPro">Retour au programme de sensibilisation au handicap AccessiPro</a></p></div>'};
 				trial.stimuli.push(media);
 				scorer.dynamicPost({
 					score1: DScoreObj.DScore,
@@ -774,7 +774,7 @@ define(['pipAPI','pipScorer'], function(APIConstructor,Scorer) {
 				});
 
 				DScoreObj = scorer.computeD();
-				media = {css:{color:'black'},media:{html:'<div><p style="font-size:18px;color=#FFFFFF>'+DScoreObj.FBMsg+'<br><span style="color:#56C2ED;">Votre résultat :'+DScoreObj.DScore+'</span></p></div><div><p style="font-size:28px;color:#FFFFFF">Vous avez terminé le test<br/><br/>Vous pouvez poursuivre le programme AccessiPro en cliquant sur ce lien :<br/><br/> <a href="http://bit.ly/2arXTSD" title="Retour au programme de sensibilisation au handicap AccessiPro">Retour au programme de sensibilisation au handicap AccessiPro</a></p></div>'}};
+				media = {css:{color:'black'},media:{html:'<div><p style="font-size:14px;color=#FFFFFF>'+DScoreObj.FBMsg+'<br><span style="color:#56C2ED;">Votre résultat :'+DScoreObj.DScore+'</span><br/><br/></p><p style="font-size:14px;color:#FFFFFF">Vous avez terminé le test<br/><br/>Vous pouvez poursuivre le programme AccessiPro en cliquant sur ce lien :<br/><br/> <a href="http://bit.ly/2arXTSD" title="Retour au programme de sensibilisation au handicap AccessiPro">Retour au programme de sensibilisation au handicap AccessiPro</a></p></div>'};
 				trial.stimuli.push(media);
 				scorer.dynamicPost({
 					score1: DScoreObj.DScore,
@@ -790,7 +790,7 @@ define(['pipAPI','pipScorer'], function(APIConstructor,Scorer) {
 				{//The instructions stimulus
 					data : {'handle':'instStim'},
 					css: {color:'black'},
-					media:{html:'<div><p style="font-size:28px;color:#FFFFFF">Vous avez terminé le test<br/><br/>Vous pouvez poursuivre le programme AccessiPro en cliquant sur ce lien :<br/><br/> <a href="http://bit.ly/2arXTSD" title="Retour au programme de sensibilisation au handicap AccessiPro">Retour au programme de sensibilisation au handicap AccessiPro</a></p></div>'}
+					media:{html:'<div><p style="font-size:14px;color:#FFFFFF">Vous avez terminé le test<br/><br/>Vous pouvez poursuivre le programme AccessiPro en cliquant sur ce lien :<br/><br/> <a href="http://bit.ly/2arXTSD" title="Retour au programme de sensibilisation au handicap AccessiPro">Retour au programme de sensibilisation au handicap AccessiPro</a></p></div>'}
 				}
 			]
 		}
