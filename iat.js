8e224835eab4c766208daf051b2eecf5badc8034
DI-access <direction-innovation@cned.fr>
2016-12-13 11:17:12 +0100

    Update iat.js

--- iat.js
+++ iat.js
@@ -778,9 +778,9 @@ define(['pipAPI','pipScorer'], function(APIConstructor,Scorer) {
 				});
 
 				// Second parcel
-				//scorer.addSettings('compute',{
-					//parcelValue : ['second']
-				//});
+				scorer.addSettings('compute',{
+					parcelValue : ['second']
+				});
 
 				//DScoreObj = scorer.computeD();
 				//media = {css:{color:'white'},media:{html:'<div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Logo_Accessi_Pro.jpg/300px-Logo_Accessi_Pro.jpg"/><p style="font-size:16px;color=#000000><span style="color:#56C2ED;">Votre résultat :'+DScoreObj.DScore+'</span><br/><br/>'+DScoreObj.FBMsg+'<br><br/></p><p style="font-size:14px;color:#000000">Continuez le programme AccessiPro en cliquant sur ce lien :<br/><br/> <a href="http://bit.ly/2arXTSD" style="color:#23A541;" title="Retour au programme de sensibilisation au handicap AccessiPro">Retour au programme de sensibilisation au handicap AccessiPro</a></p></div>'}};
